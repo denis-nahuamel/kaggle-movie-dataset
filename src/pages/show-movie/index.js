22fb756316be7a30b8e6e3cb5b927e560744c031
Denis Nahuamel <nahuamel.denis@gmail.com>
2022-10-11 16:01:23 -0500

    add new styles

--- src/pages/show-movie/index.js
+++ src/pages/show-movie/index.js
@@ -1,30 +1,44 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { Button, Dialog, DialogActions, DialogContent } from "@mui/material";
-import { BoldStyle } from "../../styles/style";
+import { BoldStyle, ColumnContainer, RowContainer } from "../../styles/style";
 import { HeaderDialog } from "./components/HeaderDialog";
 
-
-export default function MovieDetail({open, handleOpen,currentMovie}) {
-    console.log("current movie",currentMovie)
-    return (
-      <div> 
-        <Dialog
-          onClose={()=>handleOpen(false)}
-          aria-labelledby="customized-dialog-title"
-          open={open}
+export default function MovieDetail({ open, handleOpen, currentMovie }) {
+  return (
+    <div>
+      <Dialog
+        onClose={() => handleOpen(false)}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <HeaderDialog
+          id="customized-dialog-title"
+          onClose={() => handleOpen(false)}
         >
-          <HeaderDialog id="customized-dialog-title" onClose={()=>handleOpen(false)} >
           {currentMovie}
-          </HeaderDialog>
-          <DialogContent dividers>
+        </HeaderDialog>
+        <DialogContent dividers css={ColumnContainer}>
+          <div>
             <BoldStyle>Descripción: </BoldStyle>
             <div>{currentMovie?.overview}</div>
-          </DialogContent>
-          <DialogActions>
-            <Button autoFocus onClick={()=>handleOpen(false)}>
-              Cerrar
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </div>
-    );
-  }
+          </div>
+
+          <RowContainer>
+            <BoldStyle>Rating: </BoldStyle>
+            {currentMovie?.vote_average}
+          </RowContainer>
+          <RowContainer>
+            <BoldStyle>Dirección: </BoldStyle>
+            <a href={currentMovie?.homepage}>{currentMovie?.homepage}</a>
+          </RowContainer>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={() => handleOpen(false)}>
+            Cerrar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
